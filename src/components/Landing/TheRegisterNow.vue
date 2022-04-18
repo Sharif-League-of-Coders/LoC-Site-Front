a3f3a85549639e5331e6466b25f03f0194a3ba75
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2022-04-18 10:44:17 +0430

    Initialize register now frame

---
+++ src/components/Landing/TheRegisterNow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "the-register-now",
+};
+</script>
+
+<style scoped>
+.container {
+  background: url("../../assets/Register Now.svg");
+  background-size: contain;
+  background-position: top center;
+  background-repeat: no-repeat;
+  height: 60vw;
+  width: 100vw;
+}
+</style>
