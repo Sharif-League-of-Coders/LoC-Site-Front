d9756f1b865961173731ee604b86099ba172f1be
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2022-04-18 10:44:41 +0430

    Initialize sponsor section

---
+++ src/components/Landing/TheSponsors.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="sponsors">
+    <div class="container">
+      <v-img :src="require('../../assets/sponsor-logo.svg')" width="27.5vw" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#sponsors {
+  direction: rtl;
+  padding: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../../assets/Sponser.svg");
+  background-size: contain;
+  background-position: top center;
+  background-repeat: no-repeat;
+  height: 60vw;
+  width: 100vw;
+}
+</style>
