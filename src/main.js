9f144c27bc8d91ab2ecacb9758ff55a7769a55f4
saman-re <saman.rezapoor1379@yahoo.com>
2022-05-08 00:31:11 +0430

    add axios to the project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import {router} from './router/router';
+import {axiosApi} from "@/axios";
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axiosApi
 
 new Vue({
     vuetify,
