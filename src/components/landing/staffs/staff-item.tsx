2d8b2cfabe913ff8704f2cd2742441ad3e960814
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2022-06-16 21:27:07 +0430

    feat(staffs): Implement staffs section

--- src/components/landing/staffs/staff-item.tsx
+++ src/components/landing/staffs/staff-item.tsx
@@ -1,13 +1,18 @@
 import { Avatar, Stack, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 
-export function StaffItem() {
-  return <Stack alignItems='center'>
-    <Avatar sx={{ width: '350px', height: '350px' }} />
-    <Typography>
-      محمدحسین دولت‌آبادی
-    </Typography>
-    <Typography>
-      تیم فرانت
-    </Typography>
-  </Stack>
+export function StaffItem({ name, image, position }) {
+  const [src, setSrc] = useState(image)
+  useEffect(() => setSrc(image), [image])
+  return (
+    <Stack alignItems="center">
+      <Avatar
+
+        sx={{ width: '250px', height: '250px' }}
+        src={`https://locsharif.com${src}`}
+      />
+      <Typography>{name}</Typography>
+      <Typography>{position}</Typography>
+    </Stack>
+  )
 }
